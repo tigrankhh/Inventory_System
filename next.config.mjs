a9ef41977f22a7ea2e36f0577e3f0e2aad46d33b
tigrankhh <khachtik2606@gmail.com>
2026-02-03 05:31:10 +0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
+    // Это позволит собрать сайт, даже если есть мелкие ошибки в типах
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  // Мы убрали блок eslint, так как в Next.js 16 он настраивается иначе
 };
 
 export default nextConfig;
