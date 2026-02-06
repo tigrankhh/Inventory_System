f4a91ca0d3d4fafd674d74612d5da03a5818890c
tigrankhh <khachtik2606@gmail.com>
2026-02-06 11:45:47 +0400

    Update supabaseServer.ts

--- src/lib/supabaseServer.ts
+++ src/lib/supabaseServer.ts
@@ -2,7 +2,7 @@ import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
 export async function createClient() {
-  const cookieStore = await cookies() // В Next 15 это await!
+  const cookieStore = await cookies()
 
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -12,13 +12,14 @@ export async function createClient() {
         getAll() {
           return cookieStore.getAll()
         },
-        setAll(cookiesToSet) {
+        // Добавляем : any сюда, чтобы TypeScript не ругался
+        setAll(cookiesToSet: any) {
           try {
-            cookiesToSet.forEach(({ name, value, options }) =>
+            cookiesToSet.forEach(({ name, value, options }: any) =>
               cookieStore.set(name, value, options)
             )
           } catch {
-            // В Edge иногда нельзя ставить куки из серверного компонента, это нормально
+            // В Edge среде это нормально
           }
         },
       },
